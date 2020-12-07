31364f2bd76a95502ad76fed25f55a0b9cb7a2ba
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-07 14:41:51 +0330

    Update and rename README.md to s2.py

---
+++ s2.py
@@ -0,0 +1,44 @@
+# s2
+import os
+import sys
+import time
+import requests
+from colorama import Fore
+def __1__():
+    try:
+        print(Fore.GREEN + "Hello\nWelcome Back ;) ")
+        time.sleep(2)
+        url = input(Fore.YELLOW + "Enter Your Address Target ==>  " )
+        time.sleep(1)
+        if url == "" or None:
+            try:
+                print(Fore.RED + "\nOk Good Lunch ;)")
+                time.sleep(1)
+                sys.exit()
+            except:
+                pass
+        else:
+            pass
+        print(Fore.YELLOW + "\n[!] - This Is Test The Target ;)")
+        time.sleep(2)
+        print(Fore.YELLOW + "\n[!] - Pleass 3 Sec Latter ;)")
+        time.sleep(3)
+        r = requests.get("http://" + url + "/wp-content/plugins/")
+        if r.status_code == 404 or r.status_code == 500:
+            try:
+                print(Fore.RED + "\n[-] - Your Target Not WordPress ;(")
+                time.sleep(1)
+                sys.exit()
+            except:
+                pass
+        else:
+            try:
+                print(Fore.GREEN + "\n[+] - Your Target Is WordPress ;)")
+    except:
+        try:
+            print(Fore.GREEN + "\nPleass Enter You Go To The Mano")
+            input("")
+            os.system("clear")
+        except:
+            pass
+__1__()
